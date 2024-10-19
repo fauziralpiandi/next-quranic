c8d938f1f18b3481ce52fb7c50ec572bad62c053
Fauzira Alpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-10-19 20:57:32 +0000

    feat: Implement Header and Footer components

---
+++ src/components/Layouts.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+
+import { site } from '~/head'
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-center" aria-label="Header">
+      <div className="flex flex-col items-center gap-1.5">
+        <span>&#1575;&#1604;&#1602;&#1585;&#1571;&#1606;</span>
+        <Link
+          href="/"
+          className="font-medium text-mono-200"
+          aria-label={site.title}
+        >
+          {site.title}
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export function Footer() {
+  return (
+    <footer
+      className="flex-shrink-0 flex justify-center items-center"
+      aria-label="Footer"
+    >
+      <div className="text-[0.8rem] text-mono-400">
+        Copyright &copy; {new Date().getFullYear()} {site.author}
+      </div>
+    </footer>
+  )
+}
